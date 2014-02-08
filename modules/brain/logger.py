f807d6f627ffc33749f18ae8f1845cc6ed15af21
konglx <jayklx@gmail.com>
2014-02-08 12:01:02 +0800

    rm logger.py

--- modules/brain/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-import sys
-
-def init_log(default_level = logging.DEBUG):
-    logger = logging.getLogger('')
-    strm_out = logging.StreamHandler(sys.__stdout__)
-    logger.setLevel(default_level)
-    logger.addHandler(strm_out)
-
