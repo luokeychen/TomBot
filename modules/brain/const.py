700389c91f7f0e301ffe00e4141236557cafcfd4
konglx <jayklx@gmail.com>
2014-03-09 19:15:33 +0800

    add missing const.py

---
+++ modules/brain/const.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#  Copyright (C) 2014 konglx
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#  1. Redistributions of source code must retain the above copyright
+#  notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#  notice, this list of conditions and the following disclaimer in the
+#  documentation and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY konglx ''AS IS'' AND ANY
+#  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL konglx BE LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+#  The views and conclusions contained in the software and documentation
+#  are those of the authors and should not be interpreted as representing
+#  official policies, either expressedor implied, of konglx.
+#
+#  File        : const.py
+#  Author      : konglx
+#  Email       : jayklx@gmail.com
+#  Date        : 2014-03-09
+#  Description : consts
+
+DEFAULT_STYLE = {'name': 'Monospace', 'size': 10,
+                 'style': [0, 0, 0], 'color': '000000'}
+
+WARNING_STYLE = {'name': '微软雅黑', 'size': 12,
+                 'style': [0, 0, 0], 'color': 'FF7F27'}
+
+ERROR_STYLE = {'name': '微软雅黑', 'size': 12,
+               'style': [0, 0, 0], 'color': 'ED1C24'}
+
+INFO_STYLE = {'name': '微软雅黑', 'size': 10,
+              'style': [0, 0, 0], 'color': '3F48CC'}
+
+CODE_STYLE = {'name': 'Consolas', 'size': 10,
+              'style': [0, 0, 0], 'color': '000000'}
